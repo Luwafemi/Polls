823cb687f90f136af1b93006e48b119b723f2514
Luwafemi <aladefemi1@gmail.com>
2022-01-30 22:21:54 +0100

    add new tests

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Question, Choice
 
 # GET QUESTIONS AND DISPLAY THEM
@@ -19,7 +19,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         #Return the last five published questions (not including those set to be published in the future).
-        Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+       return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 
