669f91995b2fa542733da811d5bb14ad2bd40794
Luwafemi <aladefemi1@gmail.com>
2022-04-05 10:49:47 +0100

    Add a test class for Polls DetailView

--- pollster/polls/tests.py
+++ pollster/polls/tests.py
@@ -5,8 +5,7 @@ from django.urls import reverse
 from .models import Question
 
 
-# Create your tests here.
-
+# A shortcut function for creating questions
 def create_question(question_text, days):
     """
     Create a question with the given `question_text` and published the
@@ -16,6 +15,38 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
+
+# A test class for 'was_published_recently' method of the Question model
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date = time)
+        self.assertIs(future_question.was_published_recently(),False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date is 
+        later than a day ago.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date = time)
+        self.assertIs(old_question.was_published_recently(),False)
+    
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date = time)
+        self.assertIs(recent_question.was_published_recently(),True)
+
+
+# A test class for the index view of the Polls app ('polls/'). Ensures future questions are not displayed on the page
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
          """
@@ -68,30 +99,25 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse("polls:index"))
         self.assertQuerysetEqual(response.context['latest_question_list'],[past_question_1,past_question_2],)
 
-class QuestionModelTests(TestCase):
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
-        """
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date = time)
-        self.assertIs(future_question.was_published_recently(),False)
 
-    def test_was_published_recently_with_old_question(self):
+# A test class for the detail view of the Polls app ('polls/:pk'). Ensures future questions return a 404
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date is 
-        later than a day ago.
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
         """
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date = time)
-        self.assertIs(old_question.was_published_recently(),False)
-    
-    def test_was_published_recently_with_recent_question(self):
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
         """
-        was_published_recently() returns True for questions whose pub_date
-        is within the last day.
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
         """
-        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date = time)
-        self.assertIs(recent_question.was_published_recently(),True)
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
